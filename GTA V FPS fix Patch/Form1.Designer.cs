c03425952b86fc7366e5f7da33f82fdf705691fe
nope! <nope!@DESKTOP-N2OV0OF>
2017-02-16 11:59:20 +0530

    1st

--- GTA V FPS fix Patch/Form1.Designer.cs
+++ GTA V FPS fix Patch/Form1.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(462, 283);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Patch Settings.xml";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(462, 285);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "V 0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GTA V FPS fix Patch/Form1.cs
+++ GTA V FPS fix Patch/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,30 @@ namespace GTA_V_FPS_fix_Patch
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string orgFile = "settings.xml";
+                string repFile = GTA_V_FPS_fix_Patch.Resource1.settings;
+                orgFile = System.IO.Path.Combine(path, orgFile);
+                //File.SetAttributes(orgFile, FileAttributes.Normal);
+                //File.Delete(orgFile);
+               
+                bool a = Uri.IsWellFormedUriString(repFile, UriKind.RelativeOrAbsolute);
+                if(a == false)
+                {
+                    MessageBox.Show("No");
+                }
+                File.Copy(repFile, orgFile, true);
+            }
+            catch (Exception k)
+            {
+                MessageBox.Show(k.ToString());
+            }
         }
     }
 }
