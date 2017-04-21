33c918b8e7a152c3d1f41a28037d24bf623fa8b5
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2017-04-22 02:46:16 +0530

    Create a.go

---
+++ a.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+        "fmt"
+        "math"
+)
+
+// N = Limit, change N to check primes till N
+const N = 20
+
+// SieveOfEratosthenes() Main algorithm, bool type
+func SieveOfEratosthenes()[N + 1] bool {
+    var primes[N + 1] bool
+    sqrtOfn := int(math.Floor(math.Sqrt(N))+1)
+    for j := range primes {
+        primes[j] = true
+    }
+    for i := 2; i <= sqrtOfn; i++ {
+		if primes[i] {
+			for j := 2*i; j < N+1; j += i {
+				primes[j] = false;
+			}
+		}
+	}
+    return primes
+}
+
+func main() {
+    var primes[N + 1] bool
+    primes = SieveOfEratosthenes()
+    for i := 2; i <= N; i++ {
+        if primes[i] {
+            fmt.Println(i)
+        }
+    }
+}
